48833e19cf3dc4e3d17d171ed07ff8c2efe1d3e9
along12-blessing <blessingalonge577@gmail.com>
2026-08-14 21:59:57 +0100

    Delete handle.py

--- handle.py
+++
@@ -1,9 +0,0 @@
-try:
-    number = input("Enter a number: ")
-    num = int(number)
-    numb = 100/num
-    print(numb)
-except ValueError:
-    print("text not allowed")
-except ZeroDivisionError:
-    print("can not be divided by 0")
